b3a043b5bf9443ee230ad8a7a84d9b76689e0fe7
fatimagama20 <familygama20@gmail.com>
2024-01-30 20:42:44 +0000

    Add categories frontend

--- foodreceipe/routes.py
+++ foodreceipe/routes.py
@@ -6,3 +6,13 @@ from foodreceipe.models import Category, Receipe, Ingredients
 @app.route("/")
 def home():
     return render_template("receipe.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+   return render_template("add_category.html")
