7429d87c97b2445ff7d22efeefd6599b7875a63d
fatimagama20 <familygama20@gmail.com>
2024-02-08 17:05:11 +0000

    Add modal for delete option

--- foodreceipe/static/js/script.js
+++ foodreceipe/static/js/script.js
@@ -3,3 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
